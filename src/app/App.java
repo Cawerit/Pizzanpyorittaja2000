9703e75899eb65da77c161e303665410f6bcfb9a
Timo Heinonen <jootimo@gmail.com>
2015-05-20 15:06:34 +0300

    Yhteys-luokka luotu

--- src/app/App.java
+++ src/app/App.java
@@ -1,52 +1,27 @@
 package app;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class App {
+	
+	public static final Yhteys yhteys = new Yhteys();
 
-<<<<<<< HEAD
     public static void main(String[] args) {	
    
-    	
-  	  String url = "jdbc:mysql://localhost:3306/";
-	  String dbName = "pp2000";
-	  String driver = "com.mysql.jdbc.Driver";
-	  String userName = "root"; 
-	  String password = "tietue";
-	  try {
-	  Class.forName(driver).newInstance();
-	  Connection conn = DriverManager.getConnection(url+dbName,userName,password);
-	  
-	  Statement st = conn.createStatement();
-	  ResultSet res = st.executeQuery("SELECT * FROM  asiakas");
-
-	  while(res.next()){
-		  int id = res.getInt("id");
-		  String nimi = res.getString("nimi");
-		  System.out.println(id + " " + nimi);
-	  }		  
-	  conn.close();
-	  
-	  } catch (Exception e) {
-	  e.printStackTrace();
-	  }
+  	  	ResultSet res = yhteys.hae("Select * from asiakas");
+  	  	try {
+			while(res.next()){
+				int id = res.getInt("id");
+				String nimi = res.getString("nimi");
+				System.out.println(id + "\t" + nimi);
 
 
-=======
-    public static void main(String[] args) {
-    	
-    	//final String userName = "root";
-    	//final String password = "tietue";
-    	
-    	
-    	
-    	try{
-    		Class.forName("com.mysql.jdbc.Driver");
-    		System.out.println("Toimii");
-    	} catch(Exception e){
-    		System.out.println("Luokkaa ei löydy");
-    	}
->>>>>>> origin/master
-    	
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
--- src/app/Asiakas.java
+++ src/app/Asiakas.java
@@ -24,4 +24,5 @@ public class Asiakas {
 	public void setOsoite(String osoite) {
 		this.osoite = osoite;
 	}
+	
 }

beba7f913b2d5d343b0461a3f008915cf6467bca
Timo Heinonen <jootimo@gmail.com>
2015-05-20 14:22:50 +0300

    Yhteys tietokantaan luotu

--- src/app/App.java
+++ src/app/App.java
@@ -4,19 +4,33 @@ import java.sql.*;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {	
+   
     	
-    	//final String userName = "root";
-    	//final String password = "tietue";
-    	
-    	
-    	
-    	try{
-    		Class.forName("com.mysql.jdbc.Driver");
-    		System.out.println("Toimii");
-    	} catch(Exception e){
-    		System.out.println("Luokkaa ei löydy");
-    	}
+  	  String url = "jdbc:mysql://localhost:3306/";
+	  String dbName = "pp2000";
+	  String driver = "com.mysql.jdbc.Driver";
+	  String userName = "root"; 
+	  String password = "tietue";
+	  try {
+	  Class.forName(driver).newInstance();
+	  Connection conn = DriverManager.getConnection(url+dbName,userName,password);
+	  
+	  Statement st = conn.createStatement();
+	  ResultSet res = st.executeQuery("SELECT * FROM  asiakas");
+
+	  while(res.next()){
+		  int id = res.getInt("id");
+		  String nimi = res.getString("nimi");
+		  System.out.println(id + " " + nimi);
+	  }		  
+	  conn.close();
+	  
+	  } catch (Exception e) {
+	  e.printStackTrace();
+	  }
+
+
     	
     }
 }
---
+++ src/app/Asiakas.java
@@ -0,0 +1,27 @@
+package app;
+
+public class Asiakas {
+
+	private int id;
+	private String nimi;
+	private String osoite;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNimi() {
+		return nimi;
+	}
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+	public String getOsoite() {
+		return osoite;
+	}
+	public void setOsoite(String osoite) {
+		this.osoite = osoite;
+	}
+}
